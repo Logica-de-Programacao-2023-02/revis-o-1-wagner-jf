a65fc69c03573070f36c29aa689ddeef6009f947
Wagner Moreira Cavalcante Junior <wagner.cavalcantejr@dinfor.uniceub>
2023-09-25 09:02:33 -0300

    q1

--- q1/q1.go
+++ q1/q1.go
@@ -1,5 +1,7 @@
 package q1
 
+import "fmt"
+
 //Em um dia quente de verão, Pete e seu amigo Billy decidiram comprar uma melancia. Eles escolheram a maior e mais
 //saborosa, na opinião deles, e, em seguida, pesaram a fruta nas balanças, obtendo seu peso em quilos. Morrendo de sede,
 //correram para casa com a melancia e decidiram dividi-la. No entanto, enfrentaram um problema difícil.
@@ -14,5 +16,13 @@ package q1
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
 func DivideWatermelon(weight int) (bool, error) {
-	return false, nil
+	if weight <= 0 {
+		return false, fmt.Errorf("Peso errado")
+	}
+	if weight > 2 && weight%2 == 0 {
+		return true, nil
+	} else {
+		return false, nil
+	}
+
 }
